9e3d85f6e57ed1d4cf32769aa6f43a2d31ea91c2
TeM4ik-prog <167311099+TeM4ik-prog@users.noreply.github.com>
2025-02-06 01:46:53 +0300

    feat: updated posts, file changes, video added

--- client/src/components/layout/Footer.tsx
+++ client/src/components/layout/Footer.tsx
@@ -5,9 +5,9 @@ import { Button } from "../ui/Button";
 
 export const Footer: React.FC = () => {
     return (
-        <footer className="flex flex-row gap-3 bg-gray-900 text-gray-100 p-4 mt-auto">
+        <footer className="flex flex-col justify-center items-center gap-3 bg-gray-900 text-gray-100 p-4 mt-auto md:flex-row">
 
-            <Button text='TeM4ik' icon={<FaGithub size={30} />} href="https://github.com/TeM4ik-prog" />
+            <Button className="h-min" text='TeM4ik' icon={<FaGithub size={30} />} href="https://github.com/TeM4ik-prog" />
 
 
 
