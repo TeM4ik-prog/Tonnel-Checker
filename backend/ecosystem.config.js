dfe8a21bade950117689747690b7b107c361bab7
TeM4ik-prog <167311099+TeM4ik-prog@users.noreply.github.com>
2025-04-14 19:06:18 +0300

    first commit

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -9,12 +9,12 @@ module.exports = {
         max_memory_restart: '512M',
   
         env: {
-          NODE_ENV: 'development',
-          APP_URL: 'http://127.0.0.54:8080',
+          NODE_ENV: 'production',
+          APP_URL: 'http://94.231.205.125:8080/',
         },
   
         env_production: {
-          NODE_ENV: 'production',
+          NODE_ENV: 'development',
           APP_URL: 'https://rnxsk3jf-8080.euw.devtunnels.ms/',
         },
       },
