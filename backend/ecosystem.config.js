abf2759fdfb1d9c87fc17e0e200468dbc419d984
TeM4ik-prog <167311099+TeM4ik-prog@users.noreply.github.com>
2025-04-14 19:23:22 +0300

    first commit

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -1,17 +1,22 @@
-// ecosystem.config.js
 module.exports = {
     apps: [
-      {
-        name: 'tonnel-checker',
-        script: 'dist/main.js',
-        instances: 1,
-        autorestart: true,
-        watch: false,
-        max_memory_restart: '512M',
-        env: {
-          NODE_ENV: 'production'
+        {
+            name: 'tonnel-checker',
+            script: 'dist/main.js',
+            instances: 1,
+            autorestart: true,
+            watch: false,
+            max_memory_restart: '512M',
+
+            env_development: {
+                NODE_ENV: 'development',
+                APP_URL: 'https://rnxsk3jf-8080.euw.devtunnels.ms/',
+            },
+
+            env_production: {
+                NODE_ENV: 'production',
+                APP_URL: 'http://94.231.205.125:8080/',
+            },
         },
-      },
     ],
-  };
-  
+};
